fe07593abab5cefb79a05c83cd375df5440168e0
Nabeel Francis <levisdevis66@gmail.com>
2024-03-25 01:16:24 +0100

    Path finder application, generated dynamic mapping.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Project is about path finding algorithm DFS and BFS
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let initial_maze = [
+    ["wall", "wall", "wall", "wall"],
+    ["start", "path", "path", "wall"],
+    ["wall", "wall", "path", "wall"],
+    ["wall", "wall", "path", "end"],
+  ];
+  const [maze, setMaze] = useState(initial_maze);
+
+  function generateMaze(height, width) {
+    let matrix = [];
+    for (let i = 0; i < height; i++) {
+      let row = [];
+      for (let j = 0; j < width; j++) {
+        row.push("wall");
+      }
+      matrix.push(row);
+    }
+
+    function isCellValid(x, y) {
+      return (
+        y >= 0 &&
+        x >= 0 &&
+        width &&
+        y < height &&
+        x < width &&
+        matrix[y][x] === "wall"
+      );
+    }
+
+    const dirs = [
+      [0, 1],
+      [1, 0],
+      [0, -1],
+      [-1, 0],
+    ];
+
+    function curvePath(x, y) {
+      matrix[y][x] = "path";
+      const directions = dirs.sort(() => Math.random() - 0.5);
+      for (let [dx, dy] of directions) {
+        const nx = x + dx * 2;
+        const ny = y + dy * 2;
+        if (isCellValid(nx, ny)) {
+          matrix[y + dy][x + dx] = "path";
+          curvePath(nx, ny);
+        }
+      }
+    }
+
+    curvePath(1, 1);
+    matrix[1][0] = "start";
+    matrix[height - 2][width - 1] = "end";
+    setMaze(matrix);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="maze-grid">
+      <button className={"maze-button"} onClick={() => generateMaze(5, 6)}>
+        Refresh
+      </button>
+      <div className="maze">
+        {maze.map((row, i) => (
+          <div className={"row"}>
+            {row.map((cell, cellIndex) => (
+              <div className={`cell ${cell}`}></div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
